50993fa40e4a5cf56674bba6bdb5b082fd6ed386
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2025-07-01 11:52:00 +0200

    Fix Bluetooth permissions

--- app/src/main/java/pl/szczodrzynski/tracker/service/Utils.kt
+++ app/src/main/java/pl/szczodrzynski/tracker/service/Utils.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.Context
 import android.os.Build
 import androidx.core.content.PermissionChecker
+import timber.log.Timber
 
 object Utils {
 
@@ -17,13 +18,15 @@ object Utils {
 			listOf(
 				Manifest.permission.ACCESS_FINE_LOCATION,
 				Manifest.permission.BLUETOOTH,
-				Manifest.permission.BLUETOOTH_ADMIN
+				Manifest.permission.BLUETOOTH_ADMIN,
 			)
 
 	fun Context.hasBluetoothPermissions(): Boolean {
 		for (permission in getBluetoothPermissions()) {
-			if (PermissionChecker.checkSelfPermission(this, permission) != PermissionChecker.PERMISSION_GRANTED)
+			if (PermissionChecker.checkSelfPermission(this, permission) != PermissionChecker.PERMISSION_GRANTED) {
+				Timber.d("Permission $permission is not granted")
 				return false
+			}
 		}
 		return true
 	}
