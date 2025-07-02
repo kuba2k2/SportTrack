909aaa340950ad5cef9f6fc739e7b4fbb942b4ab
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2025-07-02 23:52:18 +0200

    Add bottom padding to history screen

--- app/src/main/java/pl/szczodrzynski/tracker/ui/screen/history/HistoryScreen.kt
+++ app/src/main/java/pl/szczodrzynski/tracker/ui/screen/history/HistoryScreen.kt
@@ -3,7 +3,9 @@ package pl.szczodrzynski.tracker.ui.screen.history
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -152,6 +154,10 @@ private fun HistoryList(
 					}
 				}
 			}
+		}
+
+		item("spacer") {
+			Spacer(modifier = Modifier.height(16.dp))
 		}
 	}
 }
